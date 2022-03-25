2190b193204a24e5a986426722a16918f5965277
BootsStribling <williamstriblingenterprises@gmail.com>
2022-03-25 10:18:44 -0500

    complete landing page structure

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,20 @@
-import styles from './Landing.module.css'
+import { Link } from 'react-router-dom'
 
 const Landing = ({ user }) => {
   return (
-    <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+    <main className='card full-page-card column-container'>
+      <div className='card edge-card column-container'>
+        <div className='card landing-logo center-card'>
+          <img src="../../assets/landing-logo.png" alt="vibin-logo"/>
+          <h7>When the vibe needs to change...</h7>
+        </div>
+          <h6>Welcome to Vibin!</h6>
+          <p className='margin-2'>At Vibin we're trying to get you into a band with the best vibe possible! Lets get you started! </p>
+        <div className='row-container'>
+          <div><Link to="/login">Log In</Link></div>
+          <div><Link to="/signup">Sign Up</Link></div>
+        </div>
+      </div>
     </main>
   )
 }
