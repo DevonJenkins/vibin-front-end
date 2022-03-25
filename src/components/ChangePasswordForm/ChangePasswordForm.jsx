2ec13cb9f1686b8b29a97085f6691546e92ba281
BootsStribling <williamstriblingenterprises@gmail.com>
2022-03-25 14:49:42 -0500

    fix bugs in classNames in all UI

--- src/components/ChangePasswordForm/ChangePasswordForm.jsx
+++ src/components/ChangePasswordForm/ChangePasswordForm.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import styles from './ChangePasswordForm.module.css'
 import * as authService from '../../services/authService'
 
 const ChangePasswordForm = props => {
@@ -40,10 +39,10 @@ const ChangePasswordForm = props => {
     <form
       autoComplete="off"
       onSubmit={handleSubmit}
-      className={styles.container}
+      className='{styles.container}'
     >
-      <div className={styles.inputContainer}>
-        <label htmlFor="password" className={styles.label}>Current Password</label>
+      <div className='{styles.inputContainer}'>
+        <label htmlFor="password" className='{styles.label}'>Current Password</label>
         <input
           type="password"
           autoComplete="off"
@@ -53,8 +52,8 @@ const ChangePasswordForm = props => {
           onChange={handleChange}
         />
       </div>
-      <div className={styles.inputContainer}>
-        <label htmlFor="newPassword" className={styles.label}>
+      <div className='{styles.inputContainer}'>
+        <label htmlFor="newPassword" className='{styles.label}'>
           New Password
         </label>
         <input
@@ -66,8 +65,8 @@ const ChangePasswordForm = props => {
           onChange={handleChange}
         />
       </div>
-      <div className={styles.inputContainer}>
-        <label htmlFor="newPasswordConf" className={styles.label}>
+      <div className='{styles.inputContainer}'>
+        <label htmlFor="newPasswordConf" className='{styles.label}'>
           Confirm New Password
         </label>
         <input
@@ -79,8 +78,8 @@ const ChangePasswordForm = props => {
           onChange={handleChange}
         />
       </div>
-      <div className={styles.inputContainer}>
-        <button disabled={isFormInvalid()} className={styles.button}>
+      <div className='{styles.inputContainer}'>
+        <button disabled={isFormInvalid()} className='{styles.button}'>
           Change Password
         </button>
         <Link to="/">
