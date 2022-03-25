8a37e77495b5909c43bb57676a5080e0a342e6d6
BootsStribling <williamstriblingenterprises@gmail.com>
2022-03-25 10:32:47 -0500

    complete landing page styling

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,17 +2,17 @@ import { Link } from 'react-router-dom'
 
 const Landing = ({ user }) => {
   return (
-    <main className='card full-page-card column-container'>
-      <div className='card edge-card column-container'>
-        <div className='card landing-logo center-card'>
+    <main className='card full-page-card column-container yellowbg'>
+      <div className='card edge-card column-container bluebg'>
+        <div className='card landing-logo center-card whitebg'>
           <img src="../../assets/landing-logo.png" alt="vibin-logo"/>
           <h7>When the vibe needs to change...</h7>
         </div>
-          <h6>Welcome to Vibin!</h6>
-          <p className='margin-2'>At Vibin we're trying to get you into a band with the best vibe possible! Lets get you started! </p>
+          <h6 className='whitefnt'>Welcome to Vibin!</h6>
+          <p className='margin-2 whitefnt' >At Vibin we're trying to get you into a band with the best vibe possible! Lets get you started! </p>
         <div className='row-container'>
-          <div><Link to="/login">Log In</Link></div>
-          <div><Link to="/signup">Sign Up</Link></div>
+          <div className='margin-2 whitebrdr card blackbg'><Link className='whitefnt' to="/login">Log In</Link></div>
+          <div className='margin-2 whitebrdr card blackbg'><Link className='whitefnt' to="/signup">Sign Up</Link></div>
         </div>
       </div>
     </main>
