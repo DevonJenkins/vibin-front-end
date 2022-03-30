28b93ba518f6958d93e59983ee4ad270561738a9
BootsStribling <williamstriblingenterprises@gmail.com>
2022-03-30 13:41:50 -0500

    remove unneccessary code in profile details

--- src/components/ProfileDetails/ProfileDetails.jsx
+++ src/components/ProfileDetails/ProfileDetails.jsx
@@ -2,11 +2,6 @@ import { useState, useEffect } from 'react'
 import NavBar from '../NavBar/NavBar';
 import * as profileService from '../../services/profileService'
 
-// import * as instrumentService from '../../services/instrumentService'
-// import * as genreService from '../../services/genreService'
-// import * as reviewService from '../../services/reviewService'
-
-
 const ProfileDetails = (props ) => {
   const [profile, setProfile] = useState([])
   const [instrumentData, setInstrumentData] = useState([])
